70691b1d3a94adb8142479cc9137e3e5d8f3fc88
Alberto Vega <avegam1801@alumno.ipn.mx>
2026-07-22 17:56:40 -0600

    Create 3967-EarliestFinishTimeForLandAndWaterRidesIi

---
+++ 3967-EarliestFinishTimeForLandAndWaterRidesIi/3967-EarliestFinishTimeForLandAndWaterRidesIi.py
@@ -0,0 +1,16 @@
+# Last updated: 22/7/2026, 5:56:37 p.m.
+class Solution:
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+        earliest_land = min(start + duration for start,duration in zip(landStartTime, landDuration))
+        earliest_water = min(start + duration for start,duration in zip(waterStartTime, waterDuration))
+
+        ans = math.inf
+        for i, start in enumerate(waterStartTime):
+            if start > earliest_land: ans = min(ans, waterDuration[i] + start)
+            else: ans = min(ans, earliest_land + waterDuration[i])
+
+        for i, start in enumerate(landStartTime):
+            if start > earliest_water: ans = min(ans, landDuration[i] + start)
+            else: ans = min(ans, earliest_water + landDuration[i])
+
+        return ans
