135dbac49def0ffa65aba67a42f26032e42827f6
Alberto Vega <avegam1801@alumno.ipn.mx>
2026-08-01 17:24:05 -0600

    Create 1516-TheKThLexicographicalStringOfAllHappyStringsOfLengthN

---
+++ 1516-TheKThLexicographicalStringOfAllHappyStringsOfLengthN/1516-TheKThLexicographicalStringOfAllHappyStringsOfLengthN.py
@@ -0,0 +1,10 @@
+# Last updated: 1/8/2026, 5:24:02 p.m.
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        nextLetter = {'a': 'bc', 'b': 'ac', 'c': 'ab'} 
+        q = collections.deque(['a', 'b', 'c'])
+        while len(q[0]) != n:
+            u = q.popleft()    
+            for v in nextLetter[u[-1]]:
+                q.append(u + v)
+        return q[k - 1] if len(q) >= k else ''    
