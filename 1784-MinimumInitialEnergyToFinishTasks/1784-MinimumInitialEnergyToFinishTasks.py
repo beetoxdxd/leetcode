82d1e32d5970669a78daebe67b3d4b106ec725e8
Alberto Vega <avegam1801@alumno.ipn.mx>
2026-08-01 17:23:13 -0600

    Create 1784-MinimumInitialEnergyToFinishTasks

---
+++ 1784-MinimumInitialEnergyToFinishTasks/1784-MinimumInitialEnergyToFinishTasks.py
@@ -0,0 +1,19 @@
+# Last updated: 1/8/2026, 5:23:10 p.m.
+class Solution:
+    def minimumEffort(self, shop: List[List[int]]) -> int:
+        shop.sort(key=lambda x: x[1] - x[0], reverse=True)
+        
+        start = shop[0][1]
+        bal = shop[0][1] - shop[0][0]
+        loan = 0
+
+        for i in range(1, len(shop)):
+            cost, thresh = shop[i]
+            
+            if bal < thresh:
+                loan += thresh - bal
+                bal = thresh
+                
+            bal -= cost
+
+        return start + loan
