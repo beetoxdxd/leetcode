ed9199e0072b71d0f0fd3266d4fd5e6aeaa40728
Alberto Vega <avegam1801@alumno.ipn.mx>
2026-08-01 17:23:43 -0600

    Create 1603-RunningSumOf1dArray

---
+++ 1603-RunningSumOf1dArray/1603-RunningSumOf1dArray.c
@@ -0,0 +1,11 @@
+// Last updated: 1/8/2026, 5:23:40 p.m.
+int* runningSum(int* nums, int numsSize, int* returnSize){
+    int *arr=(int *)malloc(sizeof(int) * numsSize);
+    short int i;
+    
+    arr[0]=nums[0];
+    for(i=1; i < numsSize; i++) arr[i]=nums[i] + arr[i-1];
+
+    *returnSize=i;
+    return arr;
+}
