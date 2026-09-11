a658285f1878524c1efaa4f9f7ffcf6a0b439ce7
Alberto Vega <avegam1801@alumno.ipn.mx>
2026-09-10 23:06:25 -0600

    File updated at 10/9/2026, 11:06:25 p.m.

--- 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.py
+++ 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.py
@@ -1,19 +1,20 @@
-# Last updated: 10/9/2026, 11:03:22 p.m.
-1class Solution:
-2    def totalNumbers(self, digits: List[int]) -> int:
-3        cont = defaultdict(int)
-4        for digit in digits:
-5            cont[str(digit)] += 1
-6
-7        ans = 0
-8
-9        for num in range(100, 1000, 2):
-10            h = Counter(str(num))
-11
-12            flag = True
-13            for key, value in h.items():
-14                if value > cont[key]: flag = False
-15
-16            if flag: ans += 1
-17
-18        return ans
+# Last updated: 10/9/2026, 11:06:25 p.m.
+class Solution:
+    def totalNumbers(self, digits: List[int]) -> int:
+        javob=set()
+        for i in range(len(digits)):
+            for j in range(len(digits)):
+                for k in range(len(digits)):
+
+                    if i==k or i==j or k==j:
+                        continue
+
+                    if digits[i]==0:
+                        continue
+
+                    if digits[k]%2!=0:
+                        continue
+
+                    son=digits[i]*100 +digits[j]*10 +digits[k]
+                    javob.add(son)
+        return len(javob)
