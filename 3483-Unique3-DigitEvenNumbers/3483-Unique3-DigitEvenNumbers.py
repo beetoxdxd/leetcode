36a7200d4890cb6bca56d210732e00ca40ae4b48
Alberto Vega <avegam1801@alumno.ipn.mx>
2026-09-10 23:07:33 -0600

    File updated at 10/9/2026, 11:07:32 p.m.

--- 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.py
+++ 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.py
@@ -1,20 +1,22 @@
-# Last updated: 10/9/2026, 11:06:25 p.m.
-class Solution:
-    def totalNumbers(self, digits: List[int]) -> int:
-        javob=set()
-        for i in range(len(digits)):
-            for j in range(len(digits)):
-                for k in range(len(digits)):
-
-                    if i==k or i==j or k==j:
-                        continue
-
-                    if digits[i]==0:
-                        continue
-
-                    if digits[k]%2!=0:
-                        continue
-
-                    son=digits[i]*100 +digits[j]*10 +digits[k]
-                    javob.add(son)
-        return len(javob)
+# Last updated: 10/9/2026, 11:07:32 p.m.
+1class Solution:
+2    def totalNumbers(self, digits: List[int]) -> int:
+3        n = len(digits)
+4        vis = [False] * 1000
+5        ans = 0
+6
+7        for i in range(n):
+8            if digits[i] == 0:
+9                continue
+10            for j in range(n):
+11                if j == i:
+12                    continue
+13                for k in range(n):
+14                    if k == i or k == j or digits[k] % 2 != 0:
+15                        continue
+16                    x = digits[i] * 100 + digits[j] * 10 + digits[k]
+17                    if not vis[x]:
+18                        vis[x] = True
+19                        ans += 1
+20
+21        return ans
