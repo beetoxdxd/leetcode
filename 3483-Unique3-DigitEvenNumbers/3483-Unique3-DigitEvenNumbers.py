182d56ea3af65267eca110eb5470640d395b5bb9
Alberto Vega <avegam1801@alumno.ipn.mx>
2026-09-10 23:03:22 -0600

    File updated at 10/9/2026, 11:03:22 p.m.

--- 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.py
+++ 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.py
@@ -1,14 +1,14 @@
-# Last updated: 10/9/2026, 10:54:33 p.m.
+# Last updated: 10/9/2026, 11:03:22 p.m.
 1class Solution:
 2    def totalNumbers(self, digits: List[int]) -> int:
-3        cont = Counter(digits)
-4        ans = 0
-5
-6        for num in range(100, 1000, 2):
-7            h = defaultdict(int)
-8            while num > 0:
-9                h[num % 10] += 1
-10                num //= 10
+3        cont = defaultdict(int)
+4        for digit in digits:
+5            cont[str(digit)] += 1
+6
+7        ans = 0
+8
+9        for num in range(100, 1000, 2):
+10            h = Counter(str(num))
 11
 12            flag = True
 13            for key, value in h.items():
