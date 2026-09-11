f56cb16447fde0374e1c4693153c901d45283b26
Alberto Vega <avegam1801@alumno.ipn.mx>
2026-09-10 22:31:15 -0600

    File updated at 10/9/2026, 10:31:15 p.m.

--- 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.py
+++ 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.py
@@ -1,18 +1,17 @@
-# Last updated: 10/9/2026, 10:29:34 p.m.
+# Last updated: 10/9/2026, 10:31:15 p.m.
 1class Solution:
 2    def totalNumbers(self, digits: List[int]) -> int:
 3        even_numbers = set()
 4        n = len(digits)
 5
-6
-7        def traverse(num: int, index: list) -> None:
-8            if len(str(num)) == 3: 
-9                if num % 2 == 0: even_numbers.add(num)
-10                return
-11
-12            for i in range(n):
-13                if i in index: continue
-14                traverse(num*10 + digits[i], index + [i])
-15
-16        traverse(0, [])
-17        return len(even_numbers)
+6        def traverse(num: int, index: list) -> None:
+7            if len(str(num)) == 3: 
+8                if num % 2 == 0: even_numbers.add(num)
+9                return
+10
+11            for i in range(n):
+12                if i in index: continue
+13                traverse(num*10 + digits[i], index + [i])
+14
+15        traverse(0, [])
+16        return len(even_numbers)
