d124f144a3f90714f839430f9de8b1a5248a62af
Alberto Vega <avegam1801@alumno.ipn.mx>
2026-09-14 01:26:59 -0600

    File updated at 14/9/2026, 1:26:59 a.m.

--- 836-RectangleOverlap/836-RectangleOverlap.py
+++ 836-RectangleOverlap/836-RectangleOverlap.py
@@ -1,11 +1,7 @@
-# Last updated: 14/9/2026, 1:21:37 a.m.
+# Last updated: 14/9/2026, 1:26:59 a.m.
 1class Solution:
 2    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
 3        x1, y1, x2, y2 = rec1
 4        x3, y3, x4, y4 = rec2
 5
-6        if x1 <= x3 < x2 or x1 < x4 <= x2 or x3 <= x1 < x4 or x3 < x2 <= x4:
-7            if y1 <= y3 < y2 or y1 < y4 <= y2 or y3 <= y1 < y4 or y3 < y2 <= y4: return True
-8            return False
-9
-10        return False
+6        return not(x2 <= x3 or x4 <= x1 or y2 <= y3 or y4 <= y1)
