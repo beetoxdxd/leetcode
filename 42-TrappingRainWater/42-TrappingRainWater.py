bc411e968fef60b8ef95aaba673fb3056e7bbb65
Alberto Vega <avegam1801@alumno.ipn.mx>
2026-09-19 22:25:11 -0600

    File updated at 19/9/2026, 10:25:09 p.m.

--- 42-TrappingRainWater/42-TrappingRainWater.py
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -1,20 +1,15 @@
-# Last updated: 19/9/2026, 10:22:31 p.m.
+# Last updated: 19/9/2026, 10:25:09 p.m.
 1class Solution:
 2    def trap(self, height: list[int]) -> int:
-3        max_left = 0
-4        max_right = 0
-5        n = len(height)
-6        i, j = 0, n-1
-7        dp = [math.inf] * n
-8
-9        while i < n:
-10            if height[i] > max_left: max_left = height[i]
-11            if height[j] > max_right: max_right = height[j]
-12
-13            dp[i] = min(max_left - height[i], dp[i])
-14            dp[j] = min(max_right - height[j], dp[j])
-15
-16            i += 1
-17            j -= 1
-18
-19        return sum(dp)
+3        n = len(height)
+4        dp = [math.inf] * n
+5        max_left, max_right = 0, 0
+6
+7        for i in range(n):
+8            if height[i] > max_left: max_left = height[i]
+9            if height[-i-1] > max_right: max_right = height[-i-1]
+10
+11            dp[i] = min(max_left - height[i], dp[i])
+12            dp[-i-1] = min(max_right - height[-i-1], dp[-i-1])
+13
+14        return sum(dp)
