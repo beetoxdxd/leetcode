65d95e4b89cd04178ca1c5d312a930242b1c9757
Alberto Vega <avegam1801@alumno.ipn.mx>
2026-09-19 19:20:41 -0600

    File updated at 19/9/2026, 7:20:39 p.m.

--- 3498-ReverseDegreeofaString/3498-ReverseDegreeofaString.py
+++ 3498-ReverseDegreeofaString/3498-ReverseDegreeofaString.py
@@ -1,4 +1,4 @@
-# Last updated: 19/9/2026, 7:20:01 p.m.
+# Last updated: 19/9/2026, 7:20:39 p.m.
 1class Solution:
 2    def reverseDegree(self, s: str) -> int:
 3        return sum((123 - ord(char)) * (i+1) for i, char in enumerate(s))
